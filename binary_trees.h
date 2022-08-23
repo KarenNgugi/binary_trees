a937fce4094d5fb09ba8fd58f5221f9d8e8623d4
KarenNgugi <karenwngugi12@gmail.com>
2022-08-23 08:45:59 +0300

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -70,10 +71,12 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value)
+heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
+/* other */
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
