44d23822a4560dd6438417eea7101bd86839cd7a
KarenNgugi <karenwngugi12@gmail.com>
2022-08-23 10:49:03 +0300

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,18 +10,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-	if (node == NULL)
+	if ((node == NULL) || (parent == NULL))
 	{
 		free(node);
 		return (NULL);
 	}
 
-	if (parent == NULL)
-		return (NULL);
-
 	node->n = value;
 	node->parent = parent;
-	node->left = NULL;
 	node->right = NULL;
 
 	if (parent->left)
@@ -31,7 +27,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = node;
 	}
 	else
+	{
 		parent->left = node;
+		node->left = NULL;
+	}
 
 	return (node);
 	free(node);
