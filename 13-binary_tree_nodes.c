ce50b8e9e1d7337bee9b8979dad0e40e73491a3f
KarenNgugi <karenwngugi12@gmail.com>
2022-08-24 12:04:30 +0300

    unfinished task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,5 +8,17 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	return;
+	size_t left_nodes = 0;
+	size_t right_nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left != NULL) || (tree->right == NULL))
+	{
+		left_nodes = binary_tree_nodes(tree->left);
+		right_nodes = binary_tree_nodes(tree->right);
+	}
+
+	return (left_nodes + right_nodes + 1);
 }
