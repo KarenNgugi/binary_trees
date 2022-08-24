e601e6d297f224a8102b2dcb76a6272f6795dd22
KarenNgugi <karenwngugi12@gmail.com>
2022-08-24 08:55:06 +0300

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,5 +8,16 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	return;
+	size_t left_leaves = 0;
+	size_t right_leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
+
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+	return (left_leaves + right_leaves);
 }
