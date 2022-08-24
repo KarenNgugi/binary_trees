0f1e6e4e89da21c54352536a2eabd6a57c715988
KarenNgugi <karenwngugi12@gmail.com>
2022-08-24 16:37:31 +0300

    task 24

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -8,5 +8,17 @@
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	return;
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left != NULL) && (tree->left->n > tree->n))
+		return (0);
+
+	if ((tree->right != NULL) && (tree->right->n < tree->n))
+		return(0);
+
+	if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
+		return (0);
+
+	return (1);
 }
