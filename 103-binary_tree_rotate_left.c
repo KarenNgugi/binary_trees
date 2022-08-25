78cb5751cfd3ed326ee9df7a2843b3d8d8e3bc6c
KarenNgugi <karenwngugi12@gmail.com>
2022-08-25 21:43:09 +0300

    task 22 - unfinished

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,5 +8,26 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	return;
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+		free(node);
+	}
+
+	if (tree == NULL )
+		return (tree);
+
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (tree);
+
+	node = binary_tree_rotate_left(tree->left);
+
+	tree->left->left = tree->right;
+	tree->left->right = tree;
+	tree->left = NULL;
+	tree->right = NULL;
+
+	return (node);
 }
