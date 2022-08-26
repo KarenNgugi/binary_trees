246dfd134a789a9de7748f5fa7221abb3b9b8a65
KarenNgugi <karenwngugi12@gmail.com>
2022-08-26 22:55:39 +0300

    task 25 - incomplete

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,16 @@
 #include "binary_trees.h"
 
+binary_tree_t *new_node(int val)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	node->n = val;
+	node->left = node->right = NULL;
+
+	return (node);
+
+}
+
 /**
  * bst_insert - inserts value in Binary Search Tree
  * @tree: double pointer of root node of BST
@@ -9,5 +20,13 @@
 
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	return;
+	if (*tree == NULL)
+		return (binary_tree_node(NULL, value));
+
+	if (value < (*tree)->n)
+		(*tree)->left = bst_insert((bst_t **)(*tree)->left, value);
+	else if (value > (*tree)->n)
+		(*tree)->right = bst_insert((bst_t **)(*tree)->right, value);
+
+	return ((bst_t *)tree);
 }
